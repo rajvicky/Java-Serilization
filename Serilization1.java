29da290a3f6c1e20397ee859749f56dfc37708cb
rajvicky <developervikas17@gmail.com>
2020-01-02 23:12:24 +0530

    Latest Chnages

--- Serilization1.java
+++ Serilization1.java
@@ -1,6 +1,35 @@
-class Serilization1{
-    public static void main(String []args) {
-        System.out.println("Hello");
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class EngineObject implements Serializable{
+    public EngineObject(){
         
+    }
+    /**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	int a=1;
+    int b=3;
+}
+class Serilization1{
+    public static void main(String []args) throws IOException ,ClassNotFoundException {
+        EngineObject eno=new EngineObject();
+        File f=new File("engineObject.txt");
+        FileOutputStream fos=new FileOutputStream(f);
+        ObjectOutputStream objs=new ObjectOutputStream(fos);
+        objs.writeObject(eno);
+        objs.close();
+        FileInputStream fis=new FileInputStream(f);
+        ObjectInputStream Obin=new ObjectInputStream(fis);
+        EngineObject d=(EngineObject) Obin.readObject();
+        System.out.println(d.a+" "+d.b);
+        Obin.close();
+        System.out.println("Hello");    
     }
 }
